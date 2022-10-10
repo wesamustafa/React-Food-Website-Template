9283c71c0805ef3a0411adb37877da0446d2474d
wesamustafa <wesamustafa@gmail.com>
2022-10-10 14:03:23 +0200

    impl number section and pride section and improvment navbar component

--- README.old.md
+++ README.old.md
@@ -1,2 +1,6 @@
 # react-food-website-template
 Create Food Website Template Using Reactjs
+
+### bootstrap
+bootstrap@5.2.2"
+react-bootstrap@2.5.0
--- src/component/Home.js/Home.js
+++ src/component/Home.js/Home.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Header from "./Header";
-
+import "./Home.css";
+import {Button} from 'react-bootstrap';
+import prideImg from '../../assets/pride.png';
 const Home = ()=>{
     return(
+        <Fragment>
         <Header/>
+        {/* start section number */}
+        <section className="numbers">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-3">
+                    <h2>1525+</h2>
+                    <h6>saving</h6>
+                </div>
+                <div className="col-md-3">
+                    <h2>2588+</h2>
+                    <h6>photos</h6>
+                </div>
+                <div className="col-md-3">
+                    <h2>1525+</h2>
+                    <h6>rockets</h6>
+                </div>
+                <div className="col-md-3">
+                    <h2>1525+</h2>
+                    <h6>globes</h6>
+                </div>
+            </div>
+        </div>
+        </section>
+        {/* End Section numbers */}
+        {/* start section pride */}
+        <div className="pride">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src={prideImg} alt="pride"/>
+                    </div>
+                    <div className="col-md-6">
+                        <h2>We pride ourselves on making real food from the best ingredients.</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum in sit amet leo. Mauris feugiat erat tellus.</p>
+                        <Button variant="primary">order now</Button>{' '}
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* end section pride */}
+        </Fragment>
     )
 }
 
