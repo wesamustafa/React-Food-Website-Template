8c27baf4e749781613973629ee6bd77fb182c097
wesamustafa <wesamustafa@gmail.com>
2022-10-10 15:17:52 +0200

    imp paralex section

--- src/component/Home.js/Home.js
+++ src/component/Home.js/Home.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import "./Home.css";
 import {Button} from 'react-bootstrap';
 import prideImg from '../../assets/pride.png';
+import ingredients from '../../assets/ingredients.png';
 const Home = ()=>{
     return(
         <Fragment>
@@ -32,7 +33,7 @@ const Home = ()=>{
         </section>
         {/* End Section numbers */}
         {/* start section pride */}
-        <div className="pride">
+        <section className="pride">
             <div className="container">
                 <div className="row">
                     <div className="col-md-6">
@@ -45,8 +46,43 @@ const Home = ()=>{
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
         {/* end section pride */}
+        {/* start section ingredients */}
+        <section className="ingredients">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h2>We make everything by hand with the best possible ingredients.</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum in sit amet leo. Mauris feugiat erat tellus.Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <ul>
+                            <li>Etiam sed dolor ac diam volutpat.</li>
+                            <li>Erat volutpat aliquet imperdiet.</li>
+                            <li>purus a odio finibus bibendum.</li>
+                        </ul>
+                        <Button variant="primary">learn more</Button>{' '}
+                    </div>
+                    <div className="col-md-6">
+                        <img src={ingredients} alt="ingredients"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* end section ingredients */}
+        {/* start section paralex */}
+        <section className="paralex">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 col-md-12">
+                        <h2>When a man's stomach is full it makes no<br/> difference whether he is rich or poor.</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio <br/>finibus bibendum in sit amet leo. Mauris feugiat erat tellus.</p>
+                        <Button variant="link">Watch Our Story</Button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* end section paralex */}
+        
         </Fragment>
     )
 }
